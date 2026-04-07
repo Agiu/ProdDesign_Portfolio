83ac99813588f3d1d19a3466553db40f535dfb1e
Agui <ccaleba@gmail.com>
2026-04-06 17:30:24 -0700

    loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { CaseStudyPage } from './pages/CaseStudyPage';
 import { Footer } from './components/Footer';
+import { UnderConstruction } from './components/UnderConstruction';
 
 export default function App() {
   const [darkColor, setDarkColor] = useState('#020510');
@@ -35,13 +36,15 @@ export default function App() {
   return (
     <Router>
       <div className="w-full min-h-screen flex flex-col">
-        <div className="flex-1">
+        <UnderConstruction />
+
+        {/* <div className="flex-1">
           <Routes>
             <Route path="/" element={<HomePage darkColor={darkColor} onDarkColorChange={handleDarkColorChange} />} />
             <Route path="/case-study/:id" element={<CaseStudyPage darkColor={darkColor} />} />
           </Routes>
         </div>
-        <Footer darkColor={darkColor} />
+        <Footer darkColor={darkColor} /> */}
       </div>
     </Router>
   );
